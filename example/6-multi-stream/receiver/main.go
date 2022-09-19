5e1e86693257f19bc43d0356e1e0838c606c89ef
C.C <fanweixiao@gmail.com>
2022-09-19 21:32:51 +0800

    refactor

---
+++ example/6-multi-stream/receiver/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/yomorun/yomo"
+	"github.com/yomorun/yomo/core"
+	"github.com/yomorun/yomo/core/frame"
+)
+
+var sfnName string
+var exit chan bool
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("please set the sfn name in args, f.e. go run main.go sfn-1")
+	}
+	sfnName = os.Args[1]
+	if sfnName == "" {
+		sfnName = "sink-1"
+	}
+
+	// init yomo-sfn
+	client := yomo.NewStreamFunction(
+		sfnName,
+		yomo.WithZipperAddr(os.Getenv("zipper")),
+		yomo.WithObserveDataTags(0x11),
+	)
+	defer client.Close()
+
+	// set stream handler (must set before connect)
+	client.SetStreamHandler(streamHandler)
+
+	// connect to yomo-zipper
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	exit = make(chan bool)
+
+	<-exit
+	client.Close()
+}
+
+type fileInfo struct {
+	Name string `json:"name"`
+}
+
+func streamHandler(in io.Reader) io.Reader {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	dir := filepath.Dir(ex)
+
+	// get stream frame
+	streamFrame, err := core.ParseFrame(in)
+	if err != nil {
+		log.Println("read frame error:", err)
+		return nil
+	}
+	if streamFrame.Type() != frame.TagOfStreamFrame {
+		log.Println("the frame type is not TagOfStreamFrame")
+		return nil
+	}
+
+	// get file info from stream frame
+	var info fileInfo
+	err = json.Unmarshal(streamFrame.(*frame.StreamFrame).Metadata(), &info)
+	if err != nil {
+		log.Println("unmarshal the metadata in stream frame error:", err)
+		return nil
+	}
+
+	log.Println("receiving file:", info.Name)
+
+	// calculate the md5 of the file
+	pipeReader, pipeWriter := io.Pipe()
+	defer pipeReader.Close()
+	defer pipeWriter.Close()
+
+	go calculateMD5(pipeReader, info.Name)
+
+	// create output file
+	p := path.Join(dir, sfnName+"-"+info.Name)
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	written, err := io.Copy(io.MultiWriter(pipeWriter, f), in)
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+	pipeWriter.Close()
+	log.Printf("written: %d, %s\n", written, p)
+
+	// signal to exit
+	exit <- true
+	return nil
+}
+
+// calculateMD5 calculates the md5 of the file.
+func calculateMD5(reader io.Reader, fileName string) {
+	h := md5.New()
+	if _, err := io.Copy(h, reader); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("file: %s, md5: %x\n", fileName, h.Sum(nil))
+}
---
+++ example/6-multi-stream/sender/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/yomorun/yomo"
+	"github.com/yomorun/yomo/core/frame"
+)
+
+var exit chan bool
+
+type fileInfo struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("please indicate the filename in args, e.g. zipper=localhost:9000 go run main.go /path/to/file")
+	}
+
+	// open file which needs to be sent
+	fp, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	fi, err := fp.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if fi.IsDir() {
+		panic("send files, not dir")
+	}
+
+	// init yomo-source
+	client := yomo.NewSource(
+		"source",
+		yomo.WithZipperAddr(os.Getenv("zipper")),
+		yomo.WithObserveDataTags(0x10),
+	)
+	defer client.Close()
+
+	// connect to yomo-zipper
+	err = client.Connect()
+	if err != nil {
+		panic(err)
+	}
+	// PROBLEM: how to wait util the connection is established?
+	time.Sleep(time.Second)
+
+	go sendFile(client, fp.Name())
+
+	exit = make(chan bool)
+
+	<-exit
+	client.Close()
+}
+
+// sendFile sends the file to yomo-zipper.
+func sendFile(client yomo.Source, fileName string) {
+	log.Printf(">>>>>sending file %s to yomo-zipper...", fileName)
+	videoStream, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer videoStream.Close()
+
+	// open a new stream
+	ctx := context.Background()
+	writer, err := client.OpenStream(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer writer.Close()
+
+	// send a stream frame with file name
+	f := frame.NewStreamFrame([]byte{0x11})
+	info := fileInfo{
+		Name: filepath.Base(fileName),
+	}
+	meta, _ := json.Marshal(info)
+	f.SetMetadata(meta)
+	_, err = writer.Write(f.Encode())
+	if err != nil {
+		panic(err)
+	}
+
+	// calculate the md5 of the file
+	pipeReader, pipeWriter := io.Pipe()
+	defer pipeWriter.Close()
+	defer pipeReader.Close()
+	go calculateMD5(pipeReader, fileName)
+
+	// send video stream to yomo-zipper
+	written, err := io.Copy(io.MultiWriter(pipeWriter, writer), videoStream)
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+	log.Printf("file: %s, written: %d\n", fileName, written)
+	exit <- true
+}
+
+// calculateMD5 calculates the md5 of the file.
+func calculateMD5(reader io.Reader, fileName string) {
+	h := md5.New()
+	if _, err := io.Copy(h, reader); err != nil {
+		if err != io.EOF {
+			log.Fatal(err)
+		}
+	}
+
+	log.Printf("file: %s, md5: %x\n", fileName, h.Sum(nil))
+}
